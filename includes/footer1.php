c4e892770e0499f9b904431db3165d252342c563
Carlos Alfaro Díaz <calfarod@gmail.com>
2025-12-16 01:11:30 -0300

    agrega footer ::: a contacto.php

---
+++ includes/footer1.php
@@ -0,0 +1,115 @@
+<!-- Footer versión 1 -->
+    <footer class="footer-mw">
+        <!-- Logo e íconos sociales -->
+        <div class="container-xl footer-logo-social">
+            <div class="row">
+                <div class="col-12 col-lg-6 text-left">
+                    <img src="img/logo2.jpg" alt="Logo" style="max-width:200px;">
+                </div>
+                <div class="col-12 col-lg-6 text-lg-right text-center">
+                    <!--  col-12 col-lg-6 text-center mt-3 mt-lg-0 -->
+                    <div class="footer-social-icons">
+                        <a href="javascript:void(0)" title="@instaprounap" data-bs-toggle="tooltip"><i
+                                class="bi bi-instagram"></i></a>
+                        <a href="javascript:void(0)" title="@fprounap" data-bs-toggle="tooltip"><i
+                                class="bi bi-facebook"></i></a>
+                        <a href="javascript:void(0)" title="@Xprounap" data-bs-toggle="tooltip"><i
+                                class="bi bi-twitter-x"></i></a>
+                        <a href="javascript:void(0)" title="@inprounap" data-bs-toggle="tooltip"><i
+                                class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- línea horizontal fina -->
+        <div class="footer-divider"></div>
+
+        <!-- Sección principal en 5 columnas con separadores -->
+        <div class="container-xl">
+            <div class="row footer-cols-main">
+                <!-- Solutions (columna más ancha) -->
+                <div class="col-lg-3 col-md-6 col-sm-12 footer-main-content text-left">
+                    <div class="footer-main-title">Solutiones</div>
+                    <div class="footer-item"><i class="bi bi-mortarboard-fill mr-2"></i> Educación Continua</div>
+                    <div class="footer-item"><i class="bi bi-people-fill mr-2"></i> Beneficios y Membresía</div>
+                    <div class="footer-item"><i class="bi bi-heart-fill mr-2"></i> Organización y Beneficencia</div>
+                    <div class="footer-item"><i class="bi bi-person-gear mr-2"></i> Responsabilidad Social</div>
+                </div>
+                <div class="footer-vertical-divider d-none d-lg-block"></div>
+                <!-- About -->
+                <div class="col-lg-2 col-md-6 col-sm-12 footer-main-content text-left">
+                    <div class="footer-main-title">Acerca de</div>
+                    <div class="footer-item"><i class="bi bi-info-circle mr-2"></i> Participación</div>
+                    <div class="footer-item"><i class="bi bi-puzzle mr-2"></i> Estudios y Vinculación con el Medio VcM
+                    </div>
+                    <div class="footer-item"><i class="bi bi-eye mr-2"></i> Casos de Éxito</div>
+                    <div class="footer-item"><i class="bi bi-chat-dots mr-2"></i> Testimonios</div>
+                </div>
+                <div class="footer-vertical-divider d-none d-lg-block"></div>
+                <!-- Resources -->
+                <div class="col-lg-2 col-md-6 col-sm-12 footer-main-content text-left">
+                    <div class="footer-main-title">Recursos</div>
+                    <div class="footer-item"><i class="bi bi-journals mr-2"></i> Blog</div>
+                    <div class="footer-item"><i class="bi bi-life-preserver mr-2"></i> Soporte</div>
+                    <div class="footer-item"><i class="bi bi-question-circle mr-2"></i> FAQs</div>
+                    <div class="footer-item"><i class="bi bi-envelope mr-2"></i> Contáctanos</div>
+                </div>
+                <div class="footer-vertical-divider d-none d-lg-block"></div>
+                <!-- Connect -->
+                <div class="col-lg-2 col-md-6 col-sm-12 footer-main-content text-left">
+                    <div class="footer-main-title">Conexión</div>
+                    <div class="footer-item"><i class="bi bi-telephone-inbound mr-2"></i> 569-5350-2325</div>
+                    <div class="footer-item"><i class="bi bi-envelope-check mr-2"></i> info@prounap.cl</div>
+                    <div class="footer-item"><i class="bi bi-calendar-check mr-2"></i> Lun-Vie</div>
+                    <div class="footer-item"><i class="bi bi-clock mr-2"></i> 9:00am-5:00pm CLST</div>
+                </div>
+                <div class="footer-vertical-divider d-none d-lg-block"></div>
+                <!-- Contact -->
+                <div class="col-lg-2 col-md-6 col-sm-12 footer-main-content text-left">
+                    <div class="footer-main-title">Contacto</div>
+                    <div class="footer-item"><i class="bi bi-geo-alt-fill mr-2"></i> Iquique + Chile</div>
+                    <div class="footer-item"><i class="bi bi-globe mr-2"></i> www.prounap.cl</div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Top resources en 4 columnas -->
+        <div class="footer-second-divider"></div>
+        <div class="container-xl">
+            <div class="row footer-resources-row">
+                <div class="col-md-3 col-sm-6 footer-resources-col text-left">
+                    <div class="footer-main-title">Actividades Clave</div>
+                    <div class="footer-item"><i class="bi bi-award mr-2"></i> Reconocimientos Profesionales</div>
+                    <div class="footer-item"><i class="bi bi-calendar-event mr-2"></i> Difusión de eventos y actividades
+                        sectoriales</div>
+                </div>
+                <div class="col-md-3 col-sm-6 footer-resources-col text-left">
+                    <div class="footer-main-title">&nbsp;</div>
+                    <div class="footer-item"><i class="bi bi-pc-display mr-2"></i> Compartir oportunidades laborales
+                    </div>
+                    <div class="footer-item"><i class="bi bi-diagram-2 mr-2"></i> Networking</div>
+                </div>
+                <div class="col-md-3 col-sm-6 footer-resources-col text-left">
+                    <div class="footer-main-title">&nbsp;</div>
+                    <div class="footer-item"><i class="bi bi-hospital mr-2"></i> Proyectos multidisciplinarios</div>
+                    <div class="footer-item"><i class="bi bi-book mr-2"></i> Capacitación y formación continua</div>
+                </div>
+                <div class="col-md-3 col-sm-6 footer-resources-col text-left">
+                    <div class="footer-main-title">&nbsp;</div>
+                    <div class="footer-item"><i class="bi bi-people mr-2"></i> Mentoría y acompañamiento</div>
+                    <div class="footer-item"><i class="bi bi-rocket-takeoff mr-2"></i> Intercambio de experiencias y
+                        buenas prácticas</div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Copyright & subir -->
+        <div class="footer-bottom pb-4">
+            © 2025 Prounap ::: Todos los derechos reservados.
+        </div>
+        <button id="back-to-top-btn" onclick="window.scrollTo({top:0, behavior:'smooth'})">
+            <span><i class="bi bi-chevron-up"></i></span>
+        </button>
+    </footer>
+<!-- fin Footer versión 1 -->
